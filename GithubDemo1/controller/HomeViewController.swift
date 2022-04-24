e501afdb84ea02a2081babe8cf5bb539c35afc27
Bekhruzjon Hakmirzaev <hakmirzaevbekhruzjon@gmail.com>
2022-04-24 17:33:52 +0500

    Added print line

---
+++ GithubDemo1/controller/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  GithubDemo1
+//
+//  Created by Bekhruz Hakmirzaev on 24/04/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Welcome to Storyboard")
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
